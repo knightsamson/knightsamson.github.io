d33babe946c62c35019317d5eed8861fed53bf9a
knightsamson <118357742+knightsamson@users.noreply.github.com>
2024-05-22 17:43:30 -0700

    Delete RowingClubWebsite-main/selenium_tests directory

--- RowingClubWebsite-main/selenium_tests/team_members_page.py
+++
@@ -1,9 +0,0 @@
-# import webdriver
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import datetime 
-from test_harness import run_tests, PATH_STRING
-
-# team members page has no custom testing functionality so we just pass it an empty function
-if __name__ == "__main__":
-	run_tests(lambda x: None, PATH_STRING+"/members")
